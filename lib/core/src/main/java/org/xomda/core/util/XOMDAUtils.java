ced618584b7bd0932bf4a9fd383635fae94e58d8
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-02-07 10:21:08 +0100

    [core] XOMDAUtils: added @SuppressWarnings("rawtypes")

--- lib/core/src/main/java/org/xomda/core/util/XOMDAUtils.java
+++ lib/core/src/main/java/org/xomda/core/util/XOMDAUtils.java
@@ -42,21 +42,22 @@ public class XOMDAUtils {
 		return null == col ? Stream.empty() : col.stream();
 	}
 
-	public static boolean isExtensionClass(Class<?> o) {
+	public static boolean isExtensionClass(final Class<?> o) {
 		return getGenericInterfaceClasses(o).anyMatch(XOmdaExtension.class::isAssignableFrom);
 	}
 
-	public static boolean isTemplateClass(Class<?> o) {
+	public static boolean isTemplateClass(final Class<?> o) {
 		return getExtensionTypes(o, Template.class).anyMatch(Predicates.alwaysTrue());
 	}
 
-	public static boolean isTemplateClass(Class<?> o, Class<?> ext) {
+	@SuppressWarnings("rawtypes")
+	public static boolean isTemplateClass(final Class<?> o, final Class<?> ext) {
 		return getExtensionTypes(o, Template.class)
 				.map(Class.class::cast)
-				.anyMatch((Class c) -> hasGeneric(c, ext));
+				.anyMatch((final Class c) -> hasGeneric(c, ext));
 	}
 
-	static boolean hasGeneric(Class<?> c, Class<?> generic) {
+	static boolean hasGeneric(final Class<?> c, final Class<?> generic) {
 		return getGenericInterfaces(c)
 				.filter(ParameterizedType.class::isInstance)
 				.map(ParameterizedType.class::cast)
@@ -67,12 +68,12 @@ public class XOMDAUtils {
 				);
 	}
 
-	public static <E extends XOmdaExtension> Stream<?> getExtensionTypes(Class<?> o, Class<E> ext) {
+	public static <E extends XOmdaExtension> Stream<?> getExtensionTypes(final Class<?> o, final Class<E> ext) {
 		return getGenericInterfaceClasses(o)
 				.filter(ext::isAssignableFrom);
 	}
 
-	static Stream<Type> getGenericInterfaces(Type t) {
+	static Stream<Type> getGenericInterfaces(final Type t) {
 		return ReflectionUtils.unchecked(Stream.concat(
 				Stream.concat(
 						Stream.of(t),
@@ -83,13 +84,12 @@ public class XOMDAUtils {
 								.flatMap(XOMDAUtils::getGenericInterfaces)
 				),
 
-				t instanceof Class tc ?
-						getGenericInterfaceClasses((tc).getSuperclass())
+				t instanceof final Class tc ? getGenericInterfaceClasses((tc).getSuperclass())
 						: Stream.empty()
 		));
 	}
 
-	static Stream<Class<?>> getGenericInterfaceClasses(Class<?> o) {
+	static Stream<Class<?>> getGenericInterfaceClasses(final Class<?> o) {
 		return ReflectionUtils.unchecked(getGenericInterfaces(o)
 				.filter(Class.class::isInstance)
 				.map(Class.class::cast));
