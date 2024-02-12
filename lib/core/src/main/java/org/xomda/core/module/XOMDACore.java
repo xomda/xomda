8aaf3938933d15687334a4dc5eebaa3b34f1b796
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-02-12 19:04:32 +0100

    XOMDA module

--- lib/core/src/main/java/org/xomda/core/module/XOMDACore.java
+++ lib/core/src/main/java/org/xomda/core/module/XOMDACore.java
@@ -1,12 +1,13 @@
 package org.xomda.core.module;
 
 import java.io.Serial;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 import org.xomda.core.extension.XOMDAExtension;
 import org.xomda.core.extension.XOMDAModule;
+import org.xomda.core.module.template.XOMDACodeTemplate;
 
-public class XOMDACore extends HashSet<Class<? extends XOMDAExtension>> implements XOMDAModule {
+public class XOMDACore extends LinkedHashSet<Class<? extends XOMDAExtension>> implements XOMDAModule {
 
 	@Serial
 	private static final long serialVersionUID = -287571412358053236L;
@@ -17,6 +18,8 @@ public class XOMDACore extends HashSet<Class<? extends XOMDAExtension>> implemen
 		add(XOMDAReverseEntity.class);
 		// parse object references (ie "XOMDA/Core/Entity")
 		add(XOMDATypeRefs.class);
+		// code templates
+		add(XOMDACodeTemplate.class);
 	}
 
 }
