370f04a27892d530c57007a6a0ddd5faea15c21c
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-02-17 21:52:38 +0100

    maven plugin

--- plugin/maven/src/main/java/org/xomda/plugin/maven/XOMDAMojo.java
+++ plugin/maven/src/main/java/org/xomda/plugin/maven/XOMDAMojo.java
@@ -1,23 +1,41 @@
 package org.xomda.plugin.maven;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "XOMDA-plugin", defaultPhase = LifecyclePhase.COMPILE)
+@Mojo(
+		name = "XOMDA-plugin",
+		defaultPhase = LifecyclePhase.COMPILE,
+		requiresDependencyResolution = ResolutionScope.COMPILE
+)
 public class XOMDAMojo extends AbstractMojo {
 
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
-	MavenProject project;
+	private MavenProject project;
 
-	@Parameter(property = "scope")
-	String scope;
+	@Parameter(property = "classpath", defaultValue = "")
+	private Set<String> classpath = new HashSet<>();
+
+	@Parameter(property = "models")
+	private Set<String> models = new HashSet<>();
+
+	@Parameter(property = "plugins")
+	private Set<String> plugins = new HashSet<>();
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		@SuppressWarnings("unchecked")
+		List<Dependency> dependencies = project.getDependencies();
 
 	}
 
---
+++ plugin/maven/src/main/java/org/xomda/plugin/maven/task/Generate.java
@@ -0,0 +1,4 @@
+package org.xomda.plugin.maven.task;
+
+public class Generate {
+}
