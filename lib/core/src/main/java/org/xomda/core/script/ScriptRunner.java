3c99060c07c2c04de9246a083b882512ae51636b
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-03-15 12:28:23 +0100

    JavaScript runner (allow host access)

--- lib/core/src/main/java/org/xomda/core/script/ScriptRunner.java
+++ lib/core/src/main/java/org/xomda/core/script/ScriptRunner.java
@@ -10,52 +10,82 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import org.slf4j.Logger;
+import org.xomda.shared.logging.LogService;
+
 public class ScriptRunner {
 
-	private static final ScriptEngineManager manager = new ScriptEngineManager();
-	private static final ScriptEngine engine = manager.getEngineByName("js");
+	private static final String JS_ENGINE = "js";
+	private static final ScriptEngineManager manager;
+	private static final ScriptEngine engine;
 
-	public static CompiledScript compile(String script) throws ScriptException {
+	static {
+		// disable stupid warnings
+		System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
+
+		// add access
+		manager = new ScriptEngineManager();
+		engine = createEngine();
+	}
+
+	public static CompiledScript compile(final String script) throws ScriptException {
 		return ((Compilable) engine).compile(script);
 	}
 
-	public static <T> Supplier<T> callable(String script) {
+	public static <T> Supplier<T> callable(final String script) {
+		final Logger logger = logger();
 		try {
-			CompiledScript compiled = compile(script);
+			final CompiledScript compiled = compile(script);
 			return (() -> {
-				ScriptEngine engine = manager.getEngineByName("js");
+				final ScriptEngine engine = createEngine();
 				try {
 					@SuppressWarnings("unchecked")
 					T result = (T) compiled.eval(engine.getContext());
 					return result;
 				} catch (ScriptException e) {
+					logger.error("Failed to invoke script", e);
 					return null;
 				}
 			});
 		} catch (ScriptException e) {
+			logger.error("Failed to load script: %s".formatted(script), e);
 			return () -> null;
 		}
 	}
 
-	public static <T> Supplier<T> callable(String script, Map<String, Object> bindings) {
+	public static <T> Supplier<T> callable(final String script, final Map<String, Object> bindings) {
+		final Logger logger = logger();
 		try {
 			CompiledScript compiled = compile(script);
-			Bindings bindingContext = engine.getBindings(ScriptContext.ENGINE_SCOPE);
-			if (null != bindings) {
-				bindingContext.putAll(bindings);
-			}
 			return (() -> {
+				final ScriptEngine engine = createEngine();
+				Bindings bindingContext = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+				if (null != bindings) {
+					bindingContext.putAll(bindings);
+				}
 				try {
 					@SuppressWarnings("unchecked")
 					T result = (T) compiled.eval(bindingContext);
 					return result;
 				} catch (ScriptException e) {
+					logger.error("Failed to invoke script", e);
 					return null;
 				}
 			});
 		} catch (ScriptException e) {
+			logger.error("Failed to load script: %s".formatted(script), e);
 			return () -> null;
 		}
+	}
+
+	private static ScriptEngine createEngine() {
+		ScriptEngine engine = manager.getEngineByName(JS_ENGINE);
+		engine.getBindings(ScriptContext.ENGINE_SCOPE).put("polyglot.js.allowHostAccess", true);
+		return engine;
+	}
+
+	private static Logger logger() {
+		return LogService.getLogger(ScriptRunner.class);
 	}
 
 }
--- lib/core/src/test/java/org/xomda/core/script/ScriptRunnerTest.java
+++ lib/core/src/test/java/org/xomda/core/script/ScriptRunnerTest.java
@@ -3,6 +3,7 @@ package org.xomda.core.script;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,23 @@ public class ScriptRunnerTest {
 
 	@Test
 	public void testScriptWithBindings() {
-		Supplier<String> supplier = ScriptRunner.callable("hello", Map.of("hello", "Hello world!"));
-		assertEquals("Hello world!", supplier.get());
+		Supplier<String> test1 = ScriptRunner.callable("hello", Map.of("hello", "Hello world!"));
+		assertEquals("Hello world!", test1.get());
+
+		Supplier<String> test2 = ScriptRunner.callable("test()", Map.of("test", (Supplier<String>) () -> "Hello world!"));
+		assertEquals("Hello world!", test2.get());
+
+		// single quote
+		Supplier<String> test3 = ScriptRunner.callable("test('Hello world!')", Map.of("test", Function.identity()));
+		assertEquals("Hello world!", test3.get());
+
+		// double quote
+		Supplier<String> test4 = ScriptRunner.callable("test(\"Hello world!\")", Map.of("test", Function.identity()));
+		assertEquals("Hello world!", test4.get());
+
+		// backtick quote
+		Supplier<String> test5 = ScriptRunner.callable("test(`Hello world!`)", Map.of("test", Function.identity()));
+		assertEquals("Hello world!", test5.get());
 	}
 
 }
