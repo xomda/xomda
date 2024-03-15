5d0b98c747a55e658868460003d19856e79fcbc2
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2024-03-15 08:49:20 +0100

    JavaScript runner with Bindings (context)

--- lib/core/src/main/java/org/xomda/core/script/ScriptRunner.java
+++ lib/core/src/main/java/org/xomda/core/script/ScriptRunner.java
@@ -1,28 +1,55 @@
 package org.xomda.core.script;
 
+import java.util.Map;
 import java.util.function.Supplier;
-import javax.script.Invocable;
+import javax.script.Bindings;
+import javax.script.Compilable;
+import javax.script.CompiledScript;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class ScriptRunner {
 
-	public static Invocable run(String script) throws ScriptException {
-		ScriptEngine engine = new ScriptEngineManager().getEngineByName("js");
-		engine.eval(script);
-		return (Invocable) engine;
+	private static final ScriptEngineManager manager = new ScriptEngineManager();
+	private static final ScriptEngine engine = manager.getEngineByName("js");
+
+	public static CompiledScript compile(String script) throws ScriptException {
+		return ((Compilable) engine).compile(script);
 	}
 
 	public static <T> Supplier<T> callable(String script) {
 		try {
-			Invocable invocable = run("function fn(){\n" + script + "\n;}");
+			CompiledScript compiled = compile(script);
+			return (() -> {
+				ScriptEngine engine = manager.getEngineByName("js");
+				try {
+					@SuppressWarnings("unchecked")
+					T result = (T) compiled.eval(engine.getContext());
+					return result;
+				} catch (ScriptException e) {
+					return null;
+				}
+			});
+		} catch (ScriptException e) {
+			return () -> null;
+		}
+	}
+
+	public static <T> Supplier<T> callable(String script, Map<String, Object> bindings) {
+		try {
+			CompiledScript compiled = compile(script);
+			Bindings bindingContext = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+			if (null != bindings) {
+				bindingContext.putAll(bindings);
+			}
 			return (() -> {
 				try {
 					@SuppressWarnings("unchecked")
-					T result = (T) invocable.invokeFunction("fn");
+					T result = (T) compiled.eval(bindingContext);
 					return result;
-				} catch (ScriptException | NoSuchMethodException e) {
+				} catch (ScriptException e) {
 					return null;
 				}
 			});
--- lib/core/src/test/java/org/xomda/core/script/ScriptRunnerTest.java
+++ lib/core/src/test/java/org/xomda/core/script/ScriptRunnerTest.java
@@ -1,16 +1,24 @@
 package org.xomda.core.script;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
 import java.util.function.Supplier;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ScriptRunnerTest {
 
 	@Test
 	public void testScript() {
-		Supplier<String> supplier = ScriptRunner.callable("return 'Hello world!'");
-		Assertions.assertEquals("Hello world!", supplier.get());
+		Supplier<String> supplier = ScriptRunner.callable("'Hello world!'");
+		assertEquals("Hello world!", supplier.get());
+	}
+
+	@Test
+	public void testScriptWithBindings() {
+		Supplier<String> supplier = ScriptRunner.callable("hello", Map.of("hello", "Hello world!"));
+		assertEquals("Hello world!", supplier.get());
 	}
 
 }
